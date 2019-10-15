3fc89c604d3255eddcc641c4ff00349d011805f6
Xun Chen <chenxun127@gmail.com>
2019-10-15 15:36:16 -0400

    don't escape html tags

--- src/View/Helper/UtilsHelper.php
+++ src/View/Helper/UtilsHelper.php
@@ -48,7 +48,7 @@ class UtilsHelper extends Helper
             $value = $this->_View->Time->format($value, null, null, $timezone);
         } elseif (empty($value)) {
             $value = "—";
-        } else {
+        } elseif ($value === strip_tags($value)) {
             $value = h($value);
         }
 
