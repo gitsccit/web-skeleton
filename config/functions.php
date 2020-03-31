2fd5a50b447010d8858c107979c0b737a290a1a2
James Chen <xchen@sourcecode.com>
2020-03-30 20:03:58 -0400

    timestamp format

--- config/functions.php
+++ config/functions.php
@@ -19,12 +19,12 @@ function endsWith($haystack, $needle)
 }
 
 /**
- * Return current unix timestamp, e.g. "1970-01-01 00:00:00"
+ * Return current unix timestamp, e.g. "1970-01-01T00:00:00"
  * @return false|string
  */
 function timestamp()
 {
-    return date("Y-m-d H:i:s");
+    return date("Y-m-d\TH:i:s");
 }
 
 /**
