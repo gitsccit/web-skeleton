e2a07813398b4e2c0cb59a505ce76d67090e94a7
James Chen <xchen@sourcecode.com>
2022-02-14 17:03:03 -0500

    files handler with /$id/$name structure

---
+++ config/FilesApiHandler.php
@@ -0,0 +1,87 @@
+<?php
+
+use Cake\Core\Configure;
+use Cake\Http\Exception\HttpException;
+use Cake\Http\Session;
+use Cake\Routing\Router;
+
+class FilesApiHandler extends ApiHandler
+{
+    protected $_tokenName = 'fileServerApiToken';
+
+    public function __construct(array $config = [])
+    {
+        parent::__construct($config);
+
+        $this->_baseUrl = Configure::read('Urls.files');
+    }
+
+    /**
+     * Gets the file info from the API using its id.
+     *
+     * @param int|string $id
+     * @return array|null
+     */
+    public function getFile($id, $name): ?array
+    {
+        $cacheKey = "file_$id";
+        $url = "/files/$id/$name";
+        $queryParams = ['refresh_callback_url' => "/pages/clear-cache/files/$id"];
+
+        return $this->cacheOrGet($cacheKey, $url, $queryParams)['file'] ?? null;
+    }
+
+    protected function getFileName($id, $name, $width = null, $height = null): string
+    {
+        $session = new Session();
+        $file = $this->getFile($id, $name);
+        $allowedSizes = $session->read('options.files.resize');
+        $allowedSizes = explode(',', $allowedSizes);
+        if ($width) {
+            foreach ($allowedSizes as $size) {
+                if ($width <= $size) {
+                    $width = (int)$size;
+                    break;
+                }
+            }
+        }
+        if ($height) {
+            foreach ($allowedSizes as $size) {
+                if ($height <= $size) {
+                    $height = (int)$size;
+                    break;
+                }
+            }
+        }
+
+        $fileName = $id;
+        if ($width) {
+            $fileName = "{$fileName}_$width";
+
+            if ($height) {
+                $fileName = "{$fileName}x$height";
+            }
+        }
+
+        $extension = get_file_extension_from_mime_type($file['mime_type']['name']);
+
+        return $fileName . $extension;
+    }
+
+    public function getFileUrl($id, $name, ?int $width = null, ?int $height = null, bool $full = true): string
+    {
+        $defaultImage = 'data:image/gif;base64,R0lGODlhAQABAAD/ACwAAAAAAQABAAACADs=';
+
+        if (empty($id) || empty($name)) {
+            return $defaultImage;
+        }
+
+        try {
+            $fileName = $this->getFileName($id, $name, $width, $height);
+        } catch (HttpException $exception) {
+            return $defaultImage;
+        }
+
+        return $this->_baseUrl/$fileName;
+    }
+}
