1802140ba49db23a2ccc2861dfaf58ae2b8886b1
James Chen <xchen@sourcecode.com>
2022-02-15 10:00:06 -0500

    files handler with /$id structure

--- config/FilesApiHandler.php
+++ config/FilesApiHandler.php
@@ -22,19 +22,19 @@ class FilesApiHandler extends ApiHandler
      * @param int|string $id
      * @return array|null
      */
-    public function getFile($id, $name): ?array
+    public function getFile($id): ?array
     {
         $cacheKey = "file_$id";
-        $url = "/files/$id/$name";
+        $url = "/files/$id";
         $queryParams = ['refresh_callback_url' => "/pages/clear-cache/files/$id"];
 
         return $this->cacheOrGet($cacheKey, $url, $queryParams)['file'] ?? null;
     }
 
-    protected function getFileName($id, $name, $width = null, $height = null): string
+    protected function getFileName($id, $width = null, $height = null): string
     {
         $session = new Session();
-        $file = $this->getFile($id, $name);
+        $file = $this->getFile($id);
         $allowedSizes = $session->read('options.files.resize');
         $allowedSizes = explode(',', $allowedSizes);
         if ($width) {
@@ -68,16 +68,16 @@ class FilesApiHandler extends ApiHandler
         return $fileName . $extension;
     }
 
-    public function getFileUrl($id, $name, ?int $width = null, ?int $height = null, bool $full = true): string
+    public function getFileUrl($id, ?int $width = null, ?int $height = null, bool $full = true): string
     {
         $defaultImage = 'data:image/gif;base64,R0lGODlhAQABAAD/ACwAAAAAAQABAAACADs=';
 
-        if (empty($id) || empty($name)) {
+        if (empty($id)) {
             return $defaultImage;
         }
 
         try {
-            $fileName = $this->getFileName($id, $name, $width, $height);
+            $fileName = $this->getFileName($id, $width, $height);
         } catch (HttpException $exception) {
             return $defaultImage;
         }
